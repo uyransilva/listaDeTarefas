53c1bd134fdc84845efce5481e6822bb34caf611
uyransilva <uyran.silva@hotmail.com>
2022-07-09 21:00:57 -0300

    Segundo commit com ajustes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# listaDeTarefas
+Olá!
+
+Esse projeto consiste em uma lista de tarefas que:
+    Cadastra nova tarefa
+    Ao selecionar a caixa, define que a tarefa foi realizada e risca a mesma
+    
--- sricpt.js
+++ sricpt.js
@@ -1,4 +1,5 @@
 const body = document.querySelector('body');
+const divGeral = document.querySelector('.geral')
 const divLista = document.querySelector('ul');
 
 const inputTexto = document.querySelector('input');
@@ -6,26 +7,33 @@ const inputTexto = document.querySelector('input');
 
 inputTexto.addEventListener('keydown', (evento) => {
     if(evento.key != 'Enter'){
-        return
+        return;
+    }
+    if(inputTexto.value == ''){
+        return;
     }
     
-    divLista.style = '.lista'
     const boxLista = document.createElement('input');
     boxLista.type = 'checkbox';
+    boxLista.classList.add('checkbox')
 
     const itemLista = document.createElement('li');
     itemLista.textContent = inputTexto.value;
 
-    
+    const quebra = document.createElement('div');
+    quebra.classList.add('break');
 
-    divLista.append(boxLista , itemLista);
-    body.append(divLista);
+    divLista.append(boxLista , itemLista, quebra);
+    divGeral.append(divLista);
+    body.append(divGeral);
 
     inputTexto.value = '';
 
     boxLista.addEventListener('click', () => {
         itemLista.style = 'text-decoration: line-through';
-        inputData.style = 'background-color: green'
-        inputData.color = 'color: white'
+    });
+
+    boxLista.removeEventListener('selectionchange',() => {
+
     });
 })
